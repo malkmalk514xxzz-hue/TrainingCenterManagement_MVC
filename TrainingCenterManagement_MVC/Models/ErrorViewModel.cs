844102d83312ffaed699ac0ffb863dd5583e0aa8
Samer Habboush <Samer Habboush@DESKTOP-B9S3DC5>
2025-07-27 03:18:51 +0300

    Add project files.

---
+++ TrainingCenterManagement_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrainingCenterManagement_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
