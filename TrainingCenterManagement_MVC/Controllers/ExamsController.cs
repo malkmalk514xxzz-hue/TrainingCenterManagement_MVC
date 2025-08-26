a34b3e0b501f56e3cc9ea3366b078bc4975930f7
Ahmad Shoriqee <Ahmad Shoriqee@DESKTOP-RPDSES4>
2025-08-26 15:37:10 +0300

    Add ExamsController & Views

---
+++ TrainingCenterManagement_MVC/Controllers/ExamsController.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TrainingCenterManagement_MVC.Data;
+using TrainingCenterManagement_MVC.Models;
+
+namespace TrainingCenterManagement_MVC.Controllers
+{
+    public class ExamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Exams
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Exams.Include(e => e.Course);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Exams/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams
+                .Include(e => e.Course)
+                .FirstOrDefaultAsync(m => m.ExamId == id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            return View(exam);
+        }
+
+        // GET: Exams/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseName");
+            return View();
+        }
+
+        // POST: Exams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ExamId,ExamName,ExamDate,IsDeleted,CourseId")] Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                exam.ExamId = Guid.NewGuid();
+                _context.Add(exam);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseName", exam.CourseId);
+            return View(exam);
+        }
+
+        // GET: Exams/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams.FindAsync(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseName", exam.CourseId);
+            return View(exam);
+        }
+
+        // POST: Exams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ExamId,ExamName,ExamDate,IsDeleted,CourseId")] Exam exam)
+        {
+            if (id != exam.ExamId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exam);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExamExists(exam.ExamId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseName", exam.CourseId);
+            return View(exam);
+        }
+
+        // GET: Exams/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams
+                .Include(e => e.Course)
+                .FirstOrDefaultAsync(m => m.ExamId == id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            return View(exam);
+        }
+
+        // POST: Exams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var exam = await _context.Exams.FindAsync(id);
+            if (exam != null)
+            {
+                _context.Exams.Remove(exam);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExamExists(Guid id)
+        {
+            return _context.Exams.Any(e => e.ExamId == id);
+        }
+
+        [Authorize(Roles = "Trainee,Admin")]
+        public async Task<IActionResult> MyExams()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var trainee = await _context.Trainees
+                .FirstOrDefaultAsync(t => t.UserId == userId);
+
+            if (trainee == null) return NotFound();
+
+            var exams = await _context.Exams
+                .Include(e => e.Course)
+                .Where(e => e.Course.CourseTrainees.Any(ct => ct.TraineeId == trainee.TraineeId))
+                .OrderByDescending(e => e.ExamDate)
+                .ToListAsync();
+
+            return View(exams);
+
+        }
+
+        [Authorize(Roles = "Trainee,Admin")]
+        public async Task<IActionResult> TakeExam(Guid examId)
+        {
+            var exam = await _context.Exams
+                .Include(e => e.Course)
+                .FirstOrDefaultAsync(e => e.ExamId == examId);
+
+            if (exam == null)
+                return NotFound("الامتحان غير موجود.");
+
+            // هنا يمكن لاحقًا ربط صفحة بدء الامتحان
+            ViewBag.Message = "هنا سيتم تنفيذ منطق البدء في الامتحان.";
+            return View("TakeExam", exam);
+        }
+
+    }
+}
